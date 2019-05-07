d72257e54ccec273b08f32bea3cff9efe9860cd8
Román Roldán <romanmoran1990@gmail.com>
2019-05-07 13:39:00 +0200

    initial commit

---
+++ js/codigo.js
@@ -0,0 +1,10 @@
+var Nombre = prompt("Escribe tu nombre: ","");
+    if (Nombre == "Pepito Grillo") {
+    alert("Hola Pepito Grillo");
+            
+}
+    else {
+    alert("Hola también para usted.");
+    
+}
+	
