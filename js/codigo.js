f107f77ac10afeb11c3dcb6cad029c6bd8565918
Román Roldán <romanmoran1990@gmail.com>
2019-05-08 09:44:06 +0200

    Hola

--- js/codigo.js
+++ js/codigo.js
@@ -1,10 +1,11 @@
-var Nombre = prompt("Escribe tu nombre: ","");
-    if (Nombre == "Pepito Grillo") {
-    alert("Hola Pepito Grillo");
+let Nombre = prompt("Escribe tu nombre:"); /* Declaro variable nombre con let en la misma línea en la le pedimos al usuario que inserte su nombre */
+    if (Nombre == "Pepito Grillo") { /* Si escribo Pepito Grillo me devuelve Pepito grillo. Si se escribe PEPITO GRILLO, devuelve "Hola también para usted" */
+    alert("Hola Pepito Grillo.");
             
 }
     else {
-    alert("Hola también para usted.");
+    alert("Hola también para usted."); /* Si escribo cualquier otra cosa, simplemente me saluda */
     
 }
-	
+    
+
